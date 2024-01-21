1c36484b0d7930825ee680d2016faa5fbb189576
Kaktusowy500 <k.lewand500@gmail.com>
2024-01-21 17:01:07 +0000

    More random randomizing, new timetables generated

--- timetable_randomizer.py
+++ timetable_randomizer.py
@@ -32,19 +32,43 @@ def generate_times(start_times, end_times):
 # Generate randomized timetables
 for i in range(10):  # Change this to the number of timetables you want to generate
     df_random = df.copy()
-    df_random['Class group'] = np.random.choice(classes, size=len(df))
-    df_random['Enrolled in the shift'] = np.random.choice(
-        registered, size=len(df))
-    df_random['Day of the week'] = np.random.choice(days_of_week, size=len(df))
-    df_random['Start'], df_random['End'] = zip(
-        *[generate_times(start_times, end_times) for _ in range(len(df))])
-    df_random['Day'] = np.random.choice(days, size=len(df))
-    df_random['Characteristics of the room requested for the class'] = np.random.choice(
-        characteristics, size=len(df))
-    df_random['Classroom'] = np.random.choice(classrooms, size=len(df))
-    df_random['Capacity'] = np.random.choice(occupancies, size=len(df))
-    df_random['Classroom characteristics'] = np.random.choice(
-        actual_characteristics, size=len(df))
+    
+    # Randomize only the selected rows for each column
+    sample_rows = df_random.sample(frac=np.random.uniform(0, 1))
+    sample_rows['Class group'] = np.random.choice(classes, size=len(sample_rows))
+    df_random.loc[sample_rows.index, 'Class group'] = sample_rows['Class group']
+
+    sample_rows = df_random.sample(frac=np.random.uniform(0, 1))
+    sample_rows['Enrolled in the shift'] = np.random.choice(registered, size=len(sample_rows))
+    df_random.loc[sample_rows.index, 'Enrolled in the shift'] = sample_rows['Enrolled in the shift']
+
+    sample_rows = df_random.sample(frac=np.random.uniform(0, 1))
+    sample_rows['Day of the week'] = np.random.choice(days_of_week, size=len(sample_rows))
+    df_random.loc[sample_rows.index, 'Day of the week'] = sample_rows['Day of the week']
+
+    sample_rows = df_random.sample(frac=np.random.uniform(0, 1))
+    sample_rows['Start'], sample_rows['End'] = zip(*[generate_times(start_times, end_times) for _ in range(len(sample_rows))])
+    df_random.loc[sample_rows.index, ['Start', 'End']] = sample_rows[['Start', 'End']]
+
+    sample_rows = df_random.sample(frac=np.random.uniform(0, 1))
+    sample_rows['Day'] = np.random.choice(days, size=len(sample_rows))
+    df_random.loc[sample_rows.index, 'Day'] = sample_rows['Day']
+
+    sample_rows = df_random.sample(frac=np.random.uniform(0, 1))
+    sample_rows['Characteristics of the room requested for the class'] = np.random.choice(characteristics, size=len(sample_rows))
+    df_random.loc[sample_rows.index, 'Characteristics of the room requested for the class'] = sample_rows['Characteristics of the room requested for the class']
+
+    sample_rows = df_random.sample(frac=np.random.uniform(0, 1))
+    sample_rows['Classroom'] = np.random.choice(classrooms, size=len(sample_rows))
+    df_random.loc[sample_rows.index, 'Classroom'] = sample_rows['Classroom']
+
+    sample_rows = df_random.sample(frac=np.random.uniform(0, 1))
+    sample_rows['Capacity'] = np.random.choice(occupancies, size=len(sample_rows))
+    df_random.loc[sample_rows.index, 'Capacity'] = sample_rows['Capacity']
+
+    sample_rows = df_random.sample(frac=np.random.uniform(0, 1))
+    sample_rows['Classroom characteristics'] = np.random.choice(actual_characteristics, size=len(sample_rows))
+    df_random.loc[sample_rows.index, 'Classroom characteristics'] = sample_rows['Classroom characteristics']
+
     # Save the randomized timetable to a CSV file
-    df_random.to_csv(
-        f'data/randomized_timetables/timetable_{i}.csv', sep=';', index=False)
+    df_random.to_csv(f'data/randomized_timetables/timetable_{i}.csv', sep=';', index=False)
